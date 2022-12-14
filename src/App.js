fb7e0e0aa6d21ad8360bda1fb0c74082eaa81419
Vadim <vadimlazuk33@gmail.com>
2022-12-14 23:04:21 +0300

    pizzas from json,realize category and size switch

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React from 'react';
 
 import Header from './components/Header';
-import Categories from "./components/Categories";
-import Sort from "./components/Sort";
-import PizzaBlock from "./components/PizzaBlock";
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
+
+import pizzas from './assets/pizzas.json';
 
 import './scss/app.scss';
 
 function App() {
   return (
     <div className="wrapper">
-      <Header/>
+      <Header />
       <div className="content">
         <div className="container">
           <div className="content__top">
@@ -19,10 +21,9 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            <PizzaBlock title="Мексиканская" price={500}/>
-            <PizzaBlock title="Пепперони" price={450}/>
-            <PizzaBlock title="Охотничая" price={590}/>
-            <PizzaBlock title="Пицца-бургер" price={470}/>
+            {pizzas.map((obj) => (
+              <PizzaBlock {...obj} />
+            ))}
           </div>
         </div>
       </div>
--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,15 +1,21 @@
-function Categories(){
-    return (<div className="categories">
-    <ul>
-      <li className="active">Все</li>
-      <li>Мясные</li>
-      <li>Вегетарианская</li>
-      <li>Гриль</li>
-      <li>Острые</li>
-      <li>Закрытые</li>
-    </ul>
-  </div>
-    );
-  };
+import React from 'react';
+
+function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  const categories = ['Все', 'Мясные', 'Вегетарианские', 'Гриль', 'Острые', 'Закрытые'];
+
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((value, i) => (
+          <li onClick={() => setActiveIndex(i)} className={activeIndex === i ? 'active' : ''}>
+            {value}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 export default Categories;
