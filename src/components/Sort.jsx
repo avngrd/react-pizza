c0b11bf92788f9e1417d851b9ef02f66881f1b77
Vadim <vadimlazuk33@gmail.com>
2022-12-15 17:08:49 +0300

    sort category select

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 
 function Sort() {
   const [open, setOpen] = React.useState(false);
+  const [selected, setSelected] = React.useState(0);
+  const list = ['популярности', 'цене', 'алфавиту'];
+  const sortName = list[selected];
+
+  const onClickListItem = (i) => {
+    setSelected(i);
+    setOpen(false);
+  };
 
   return (
     <div className="sort">
@@ -18,14 +26,19 @@ function Sort() {
           />
         </svg>
         <b>Сортировка по:</b>
-        <span>популярности</span>
+        <span onClick={() => setOpen(!open)}>{sortName}</span>
       </div>
       {open && (
         <div className="sort__popup">
           <ul>
-            <li className="active">популярности</li>
-            <li>цене</li>
-            <li>алфавиту</li>
+            {list.map((name, i) => (
+              <li
+                key={i}
+                onClick={() => onClickListItem(i)}
+                className={selected === i ? 'active' : ''}>
+                {name}
+              </li>
+            ))}
           </ul>
         </div>
       )}
