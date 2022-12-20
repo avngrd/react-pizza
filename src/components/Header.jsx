cb18284039486f78e85edca73a493063ad95f115
Vadim <vadimlazuk33@gmail.com>
2022-12-20 17:25:09 +0300

    some fixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 
 import logoSvg from '../assets/img/pizza-logo.svg';
+import Search from './Search';
 
 function Header() {
   return (
@@ -15,6 +16,7 @@ function Header() {
             </div>
           </div>
         </Link>
+        <Search />
         <div className="header__cart">
           <Link to="/cart" className="button button--cart">
             <span>520 ₽</span>
