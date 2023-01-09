b04dc7ee3302fa1560674c431e8ebbf7376e8c83
Vadim <vadimlazuk33@gmail.com>
2023-01-09 20:48:02 +0300

    project is done!

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import qs from 'qs';
+// import qs from 'qs';
 import { useNavigate } from 'react-router-dom';
 
 import { SearchPizzaParams } from '../redux/pizza/types';
